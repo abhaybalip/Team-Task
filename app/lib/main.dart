7208837ac21d8b9b12895bdecaa374c8f735411a
Abhay Balip <121150307+abhaybalip@users.noreply.github.com>
2025-03-08 14:45:56 +0530

    project add :pin

---
+++ app/lib/main.dart
@@ -0,0 +1,26 @@
+
+import 'package:app/component/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyApp',
+      routes: {
+        '/': (context) => const Home()
+      },
+    );
+  }
+}
